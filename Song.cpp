894feefa162578f9a0d5966b3ce142ff281c36df
Anthony <lwatson@ithaca.edu>
2018-11-27 17:19:59 -0500

    Made the song. cpp file

---
+++ Song.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Anthony on 11/27/2018.
+//
+#include# "Song.h"
+
+
+Song::Song(){}
+
+std::string Song::getArtist(){
+
+}
+
+std::string Song::getTitle(){
+
+}
+std ::string Song::getDuration(){}
+
+int Song::getPlayCount(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
