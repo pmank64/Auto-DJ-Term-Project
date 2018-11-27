0e684fde5200b97b70bb31fe4ff065e2dc8fb918
John Hunter <jhunter1@ithaca.edu>
2018-11-27 16:47:42 -0500

    Started main.cpp structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //TODO Set up objects and stuff
+
+    std::cout << "Welcome to Auto-DJ, type help for a list of commands." << std::endl;
+    std::string command;
+    std::getline(std::cin,command);
+    bool inputRecognized = false;
+
+    while(command!="quit"){
+        inputRecognized = false;
+
+        if(command == "asd asd"){
+            inputRecognized = true;
+            std::cout << "ASDASDASD" << std::endl;
+        }
+        if(command == "qwe"){
+            std::cout << "YEEHAW" << std::endl;
+        }
+
+
+
+        if(!inputRecognized){
+            std::cout << "Command not recognized, type help for a list of commands." << std::endl;
+        }
+
+        std::getline(std::cin,command);
+    }
+
+    std::cout << "Quitting Auto-DJ" << std::endl;
     return 0;
 }
