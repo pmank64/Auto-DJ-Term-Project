ab4ede01ac805006114fd0603a94e4aa5db18ce8
Peter Mankiewich <pmank64@gmail.com>
2018-12-02 17:28:50 -0500

    Add files via upload

---
+++ PlaylistNode.h
@@ -0,0 +1,21 @@
+//
+// Created by Peter Mankiewich on 11/27/18.
+//
+
+#ifndef AUTODJ_PLAYLISTNODE_H
+#define AUTODJ_PLAYLISTNODE_H
+
+class PlaylistNode {
+private:
+//    Playlist* item;
+//    PlaylistNode* next;
+public:
+//    PlaylistNode(Playlist* item);
+//    PlaylistNode(const PlaylistNode& nodeToCopy);
+//    Playlist* getItem();
+//    PlaylistNode* getNext();
+
+};
+
+
+#endif //AUTODJ_PLAYLISTNODE_H
---
+++ SongNode.h
@@ -0,0 +1,20 @@
+//
+// Created by Peter Mankiewich on 11/27/18.
+//
+
+#ifndef AUTODJ_SONGNODE_H
+#define AUTODJ_SONGNODE_H
+
+class SongNode {
+private:
+//    Song* item;
+//    SongNode* next;
+public:
+//    SongNode(Song* item);
+//    SongNode(const SongNode& nodeToCopy);
+//    Song* getItem();
+//    SongNode* getNext();
+
+};
+
+#endif //AUTODJ_SONGNODE_H
