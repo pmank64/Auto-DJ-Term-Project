a350e238d6d7d4023660fdabb7c6871c449d3e1d
Anthony <lwatson@ithaca.edu>
2018-12-02 22:25:07 -0500

    Created playlistADT.h

---
+++ playlistLibADT.h
@@ -0,0 +1,25 @@
+//
+// Created by Anthony on 12/2/2018.
+//
+
+#ifndef AUTODJ_PLAYLISTLIBADT_H
+#define AUTODJ_PLAYLISTLIBADT_H
+
+#endif //AUTODJ_PLAYLISTLIBADT_H
+class playlistLibADT{
+private:
+
+public:
+    virtual playlistLibADT()=0;
+
+
+    virtual ~playlistLibADT()=0;
+
+    virtual void addSong(Song songToAdd)=0;
+
+    virtual removeSong(Song songToRemove)=0;
+
+    virtual std::string songsLeft()=0;
+
+
+};
